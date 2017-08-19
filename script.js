8bd1a607dfc1dcc495e4d8ea60a4d052201efef1
Nick Beaujean <niekvenlo@gmail.com>
2017-08-18 18:26:29 -0700

    improve rotate

--- script.js
+++ script.js
@@ -12,22 +12,24 @@ window.onload = function () {
       document.body.appendChild(this.canvas)
     }
     setup () {
+      // Initialize parameters
       this.blocks = []
       this.highestTetroIndex = 0
-      this.tetrominoSpawnPos = {x: 3, y: 0}
+      this.tetroSpawnPos = {x: 3, y: 4}
       this.tetroTypes = [
-        {type: 'I', x: [1, 0, 2, 3], y: [0, 0, 0, 0]},
-        {type: 'J', x: [1, 0, 2, 2], y: [0, 0, 0, 1]},
-        {type: 'L', x: [1, 0, 2, 0], y: [0, 0, 0, 1]},
-        {type: 'O', x: [1, 2, 1, 2], y: [0, 0, 1, 1]},
-        {type: 'S', x: [1, 2, 0, 1], y: [0, 0, 1, 1]},
-        {type: 'T', x: [1, 0, 2, 1], y: [0, 0, 0, 1]},
-        {type: 'Z', x: [0, 1, 1, 2], y: [0, 0, 1, 1]}
+        //     Name     X and Y position of four blocks      Color
+        {type: 'I', x: [1, 0, 2, 3], y: [0, 0, 0, 0], color: '#888'},
+        {type: 'J', x: [1, 0, 2, 2], y: [0, 0, 0, 1], color: '#999'},
+        {type: 'L', x: [1, 0, 2, 0], y: [0, 0, 0, 1], color: '#AAA'},
+        {type: 'O', x: [1, 2, 1, 2], y: [0, 0, 1, 1], color: '#BBB'},
+        {type: 'S', x: [1, 2, 0, 1], y: [0, 0, 1, 1], color: '#CCC'},
+        {type: 'T', x: [1, 0, 2, 1], y: [0, 0, 0, 1], color: '#DDD'},
+        {type: 'Z', x: [1, 0, 1, 2], y: [0, 0, 1, 1], color: '#EEE'}
       ]
-      this.newTetromino()
+      this.newTetro()
     }
-    newTetromino () {
-      this.activeTetromino = new Tetromino()
+    newTetro () {
+      this.activeTetro = new Tetro()
     }
     tick () {
       // Animate movement
@@ -37,58 +39,35 @@ window.onload = function () {
     }
     paint () {
       this.blocks.sort((a, b) => {
-        // Sorts the blocks, so that top-left most blocks are painted first
-        // Prevents overlapping shadows
+        // Sorting prevents overlapping shadows after drawing
         return (10 * a.realPos.x + a.realPos.y) - (10 * b.realPos.x + b.realPos.y)
       })
       this.blocks.forEach(block => {
+        // Draw each block
         let {x, y} = block.realPos
         let b = this.blockWidth
         this.context.shadowBlur = 10
         this.context.shadowOffsetX = this.blockWidth / 6
         this.context.shadowOffsetY = this.blockWidth / 6
         this.context.shadowColor = '#000'
-        switch (block.tetroType) {
-          case 'I':
-            this.context.fillStyle = '#888'
-            break
-          case 'J':
-            this.context.fillStyle = '#999'
-            break
-          case 'L':
-            this.context.fillStyle = '#AAA'
-            break
-          case 'O':
-            this.context.fillStyle = '#BBB'
-            break
-          case 'S':
-            this.context.fillStyle = '#CCC'
-            break
-          case 'T':
-            this.context.fillStyle = '#DDD'
-            break
-          case 'Z':
-            this.context.fillStyle = '#EEE'
-        }
+        this.context.strokeStyle = '#333'
+        let tetroType = this.tetroTypes.find((x) => x.type === block.tetroType)
+        this.context.fillStyle = tetroType.color
+        this.context.strokeRect(x * b, y * b, b, b)
         this.context.fillRect(x * b, y * b, b, b)
-        this.context.stroke()
-        // console.log(block)
-        console.log('type' + block.tetroType)
       })
     }
     get getRandomTetroType () {
       return this.tetroTypes[Math.floor(Math.random() * 7)]
     }
     canFall ({x, y, tetroIndex}) { // TODO
-      console.log(x, y, tetroIndex)
       true
     }
   }
-  class Tetromino {
+  class Tetro {
     constructor () {
       let tetroIndex = game.highestTetroIndex++
-      let x = 3
-      let y = 0
+      let {x, y} = game.tetroSpawnPos
       let tetroType = game.getRandomTetroType
       this.blocks = []
 
@@ -104,40 +83,21 @@ window.onload = function () {
       }
     }
     rotateLeft () {
-      let pivotX = this.blocks[0].realPos.x
-      let pivotY = this.blocks[0].realPos.y
-      let offsets = {
-        '-3,0': {x: 3, y: 3},
-        '-2,-1': {x: 1, y: 3},
-        '-2,0': {x: 2, y: 2},
-        '-2,1': {x: 1, y: 3},
-        '-1,-1': {x: 0, y: 2},
-        '-1,0': {x: 1, y: 1},
-        '-1,1': {x: 2, y: 0},
-        '0,-1': {x: -1, y: 1},
-        '0,-2': {x: -2, y: 2},
-        '0,-3': {x: -3, y: 3},
-        '0,0': {x: 0, y: 0},
-        '0,1': {x: 1, y: -1},
-        '1,-1': {x: -2, y: 0},
-        '1,-2': {x: -3, y: 1},
-        '1,0': {x: -1, y: -1},
-        '1,1': {x: 0, y: -2},
-        '1,2': {x: -1, y: -3},
-        '2,0': {x: -2, y: -2},
-        '2,1': {x: -1, y: -3},
-        '3,0': {x: -3, y: -3}
-      }
-      console.log('pivot', pivotX, pivotY)
+      let pivot = this.blocks[0].realPos
+      let offsets = (a, b) => { return {offsetX: b - a, offsetY: -a - b} }
       this.blocks.forEach((block, idx) => {
-        let relativeX = block.realPos.x - pivotX
-        let relativeY = block.realPos.y - pivotY
-        let rel = [relativeX, relativeY].join(',')
-        block.realPos.x += offsets[rel].x
-        block.realPos.y += offsets[rel].y
+        let relX = block.realPos.x - pivot.x
+        let relY = block.realPos.y - pivot.y
+        let {offsetX, offsetY} = offsets(relX, relY)
+        block.realPos.x += offsetX
+        block.realPos.y += offsetY
       })
     }
-
+    rotateRight () {
+      this.rotateLeft()
+      this.rotateLeft()
+      this.rotateLeft()
+    }
   }
 
   class Block {
@@ -152,5 +112,6 @@ window.onload = function () {
   // Initialize
   let game = new Game()
   game.setup()
+  game.activeTetro.rotateRight()
   game.paint()
 }
