67eac4eb2aac17a6bedca195bc0b7ec6ddf13a30
Nick Beaujean <niekvenlo@gmail.com>
2017-09-01 21:34:03 -0700

    Complete lines disappear, score is displayed. 95% Playable

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ window.onload = function () {
   class Game {
     setup () {
       // Initialize params
-      this.board = {
+      this.settings = {
         width: 10,
         height: 16,
-        spawnPos: {x: 4, y: -1}
+        spawnPos: {x: 4, y: -1},
+        timeToPaint: 50,
+        timeToFall: 600,
+        pointsValues: [0, 100, 200, 500, 800],
+        scoreColor: '#eee'
       }
+      this.score = 0
       this.allBlocks = []
-      this.tetroIndex = 0
+      this.highestIndex = 0
       this.tetroTypes = {
         'I': {pivot: {x: 1, y: 0}, x: [1, 0, 2, 3], y: [0, 0, 0, 0], color: '#42c2f4'},
         'J': {pivot: {x: 1, y: 0}, x: [1, 0, 2, 2], y: [0, 0, 0, 1], color: '#415cf4'},
@@ -23,23 +28,37 @@ window.onload = function () {
 
       this.createCanvas()
       document.addEventListener('keydown', this.keyHandler.bind(this), false)
-      this.timer = setInterval(this.tick.bind(this), 600)
+      this.timer = setInterval(this.tick.bind(this), this.settings.timeToPaint)
+      this.lastTime = Date.now()
     }
     tick () {
-      // Create a new Tetro when no blocks can fall
-      if (this.allBlocks.every(block => !this.canMove({offset: {x: 0, y: 1}}))) {
-        this.newTetro()
+      console.log(this.score)
+      if (this.lastTime + this.settings.timeToFall < Date.now()) {
+        this.lastTime = Date.now()
+        // Create a new Tetro when no blocks can fall
+        if (this.allBlocks.every(block => !this.canMove(
+          {offset: {x: 0, y: 1}, index: block.index}
+          ))) {
+          // Remove any complete lines
+          this.removeCompleteLine()
+          // Spawn new Tetro
+          this.newTetro()
+        }
+        // Try moving every block down
+        this.dropAll()
       }
-      // Move the current Tetro down
-      this.tetroMove({x: 0, y: 1})
       // Paint the game
       this.paint()
+    }
+    dropAll () {
+      let indices = Array.from(new Set(this.allBlocks.map(block => block.index)))
+      indices.forEach(idx => this.blockMove({offset: {x: 0, y: 1}, index: idx}))
     }
     newTetro () {
       // Create a Tetro, consisting of four blocks
       let type = this.getRandomTetroType
-      let index = ++this.tetroIndex
-      let s = this.board.spawnPos
+      let index = ++this.highestIndex
+      let s = this.settings.spawnPos
       let tetro = []
       for (let i = 0; i < 4; i++) {
         let pos = {x: s.x + type.x[i], y: s.y + type.y[i]}
@@ -67,13 +86,13 @@ window.onload = function () {
           this.tetroRotate()
           break
         case 'ArrowLeft':
-          this.tetroMove({x: -1, y: 0})
+          this.blockMove({offset: {x: -1, y: 0}, index: this.highestIndex})
           break
         case 'ArrowRight':
-          this.tetroMove({x: 1, y: 0})
+          this.blockMove({offset: {x: 1, y: 0}, index: this.highestIndex})
           break
         case 'ArrowDown':
-          this.tetroMove({x: 0, y: 1})
+          this.blockMove({offset: {x: 0, y: 1}, index: this.highestIndex})
           break
         default:
       }
@@ -83,14 +102,14 @@ window.onload = function () {
       this.canvas = document.createElement('canvas')
       this.context = this.canvas.getContext('2d')
       this.canvas.height = window.innerHeight
-      this.canvas.width = window.innerHeight / this.board.height * this.board.width
-      this.blockWidth = window.innerHeight / this.board.height
+      this.canvas.width = window.innerHeight / this.settings.height * this.settings.width
+      this.blockWidth = window.innerHeight / this.settings.height
       document.body.appendChild(this.canvas)
     }
     paint () {
       this.allBlocks.sort((a, b) => {
         // Sorting prevents overlapping shadows after drawing
-        return (100 * a.pos.x + a.pos.y) - (100 * b.pos.x + b.pos.y)
+        return (a.pos.x + a.pos.y) - (b.pos.x + b.pos.y)
       })
       // Clear canvas
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
@@ -107,6 +126,20 @@ window.onload = function () {
         this.context.strokeRect(x * bWidth, y * bWidth, bWidth, bWidth)
         this.context.fillRect(x * bWidth, y * bWidth, bWidth, bWidth)
       })
+      // Display score
+      if (this.score > 0) {
+        let bWidth = this.blockWidth
+        this.context.fillStyle = this.settings.scoreColor
+        this.context.font = bWidth + 'px Arial'
+        this.context.textAlign = 'right'
+        this.context.shadowOffsetX = bWidth / 16
+        this.context.shadowOffsetY = bWidth / 16
+        this.context.fillText(
+          this.score,
+          this.canvas.width * 0.9,
+          this.canvas.height * 0.1
+        )
+      }
       // If timer is unset, the game is over
       if (!this.timer) {
         let bWidth = this.blockWidth
@@ -122,44 +155,45 @@ window.onload = function () {
       }
     }
     tetroRotate () { // Left
-      let canRotate = this.canMove({rotate: true})
+      let canRotate = this.canMove({rotate: true, index: this.highestIndex})
       if (!canRotate) return false
 
-      this.currentTetro
+      this.allBlocks.filter(block => block.index === this.highestIndex)
         .forEach(block => {
           let offset = this.findRotateOffset(block)
           block.pos.x += offset.x
           block.pos.y += offset.y
         })
     }
-    tetroMove (offset) {
-      // Move all blocks in Tetro if they can move in the given direction
-      if (!this.canMove({offset})) return false
-      this.currentTetro.forEach(block => {
+    blockMove ({offset, index = null}) {
+      index = index || this.highestIndex
+      if (!this.canMove({offset, index})) return false
+      let tetro = this.allBlocks.filter(block => block.index === index)
+      tetro.forEach(block => {
         block.pos.x += offset.x
         block.pivot.x += offset.x
         block.pos.y += offset.y
         block.pivot.y += offset.y
       })
     }
-    canMove ({offset = null, rotate = false}) {
-      // Return true if current tetro can move
-      // If no offset is given, rotation is assumed
-      return this.currentTetro.every(block => {
+    canMove ({index, offset, rotate = false}) {
+      console.assert(!(index !== this.highestIndex && rotate))
+      let tetro = this.allBlocks.filter(block => block.index === index)
+      return tetro.every(block => {
         if (rotate) {
           offset = this.findRotateOffset(block)
         }
          // Find blocking blocks
         let blockingBlock = this.findBlock({
           pos: block.pos,
-          excludeIndex: this.tetroIndex,
+          excludeIndex: index,
           offset
         })
         // Check for bottom
-        let atBottom = (block.pos.y + offset.y) > this.board.height - 1
+        let atBottom = (block.pos.y + offset.y) > this.settings.height - 1
          // Check for sides
         let atLeftSide = block.pos.x + offset.x < 0
-        let atRightSide = block.pos.x + offset.x > this.board.width - 1
+        let atRightSide = block.pos.x + offset.x > this.settings.width - 1
         return !(!!blockingBlock || atBottom || atLeftSide || atRightSide)
       })
     }
@@ -182,8 +216,20 @@ window.onload = function () {
           (block.index !== excludeIndex))
       })
     }
-    get currentTetro () {
-      return this.allBlocks.filter(block => block.index === this.tetroIndex)
+    removeCompleteLine () {
+      // Count the number of blocks on each line
+      let count = {}
+      this.allBlocks.forEach(function (block) {
+        count[block.pos.y] = count[block.pos.y] + 1 || 1
+      })
+      // Select full lines
+      let lines = Object.entries(count)
+        .filter(([key, value]) => value >= this.settings.width)
+        .map(arr => +arr[0])
+      // Eliminate blocks on those lines
+      this.allBlocks = this.allBlocks.filter(block => !lines.includes(block.pos.y))
+      // Update the score
+      this.score += (this.settings.pointsValues[lines.length])
     }
     get getRandomTetroType () {
       let types = Object.keys(this.tetroTypes)
